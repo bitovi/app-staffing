82e66feae78931b29c2d741c9ec86f27516174d0
David Nicholas <dnicholas@Davids-MacBook-Pro.local>
2021-08-24 16:38:07 -0700

    Add test for useEmployees

--- src/services/api/useEmployees/useEmployees.test.ts
+++ src/services/api/useEmployees/useEmployees.test.ts
@@ -1,1 +1,15 @@
+import { renderHook } from "@testing-library/react-hooks";
 
+import useEmployees from "./useEmployees";
+import { employees } from "../fixtures";
+
+describe("useEmployees", () => {
+  it("should fetch", async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useEmployees());
+    expect(result.current.data).toBe(undefined);
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toEqual(employees);
+  });
+});
