92972f96474482ced017c90294cd9aa71ebc3124
David Nicholas <34694793+DavidNic11@users.noreply.github.com>
2021-08-24 16:50:52 -0700

    Install swr and refactor useEmployees to use it (#20)

--- src/pages/Employees/Employees.test.tsx
+++ src/pages/Employees/Employees.test.tsx
@@ -3,32 +3,34 @@ import userEvent from "@testing-library/user-event";
 
 import Employees from "./Employees";
 
-test("Employees renders", async () => {
-  render(<Employees />);
-  expect(screen.getByText("Team")).toBeInTheDocument();
-});
+describe("Pages/Employees", () => {
+  it("works", async () => {
+    render(<Employees />);
+    expect(screen.getByText("Team")).toBeInTheDocument();
+  });
 
-test("employee names render", async () => {
-  render(<Employees />);
+  it("shows names", async () => {
+    render(<Employees />);
 
-  // wait for the first row
-  expect(await screen.findByDisplayValue(/Tom/i)).toBeInTheDocument();
+    // wait for the first row
+    expect(await screen.findByDisplayValue(/Tom/i)).toBeInTheDocument();
 
-  // check the rest of the rows
-  expect(screen.getByDisplayValue(/Sally/i)).toBeInTheDocument();
-  expect(screen.getByDisplayValue(/Paul/i)).toBeInTheDocument();
-  expect(screen.getByDisplayValue(/Stephanie/i)).toBeInTheDocument();
-});
+    // check the rest of the rows
+    expect(screen.getByDisplayValue(/Sally/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/Paul/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/Stephanie/i)).toBeInTheDocument();
+  });
 
-test("filter by employee name", async () => {
-  render(<Employees />);
+  it("filters by name", async () => {
+    render(<Employees />);
 
-  // wait for the first row
-  expect(await screen.findByDisplayValue(/Tom/i)).toBeInTheDocument();
+    // wait for the first row
+    expect(await screen.findByDisplayValue(/Tom/i)).toBeInTheDocument();
 
-  // Filter by Sally
-  userEvent.type(screen.getByPlaceholderText(/Filter/i), "Sally");
+    // Filter by Sally
+    userEvent.type(screen.getByPlaceholderText(/Filter/i), "Sally");
 
-  // Make sure Tom is no longer visible
-  expect(screen.queryByDisplayValue(/Tom/i)).not.toBeInTheDocument();
+    // Make sure Tom is no longer visible
+    expect(screen.queryByDisplayValue(/Tom/i)).not.toBeInTheDocument();
+  });
 });
--- src/pages/Hello/Hello.tsx
+++ src/pages/Hello/Hello.tsx
@@ -1,21 +1,14 @@
-import type { Employee } from "../../services/api";
-
-import { useEffect, useState } from "react";
-import { getData } from "../../services/api";
+import { useEmployees } from "../../services/api";
 
 export default function Hello({ name }: { name: string }): JSX.Element {
-  const [data, setData] = useState<Employee[]>();
-
-  useEffect(() => {
-    getData(name).then(setData);
-  }, [name]);
+  const { data: employees } = useEmployees();
 
   return (
     <div>
       <h1>Hello, {name}!</h1>
-      {data && (
+      {employees && (
         <ul>
-          {data.map(({ id, name }) => (
+          {employees.map(({ id, name }) => (
             <li key={id}>{name}</li>
           ))}
         </ul>
