1bea33eefeca1a9b5deeda41aaa4474274c16181
David Nicholas <dnicholas@Davids-MacBook-Pro.local>
2021-08-24 12:54:37 -0700

    Install swr and refactor useEmployees to use it

--- src/pages/Hello/Hello.tsx
+++ src/pages/Hello/Hello.tsx
@@ -1,4 +1,4 @@
-import { useEmployees } from "../../services/api";
+import type { Employee } from "../../services/api";
 
 export default function Hello({ name }: { name: string }): JSX.Element {
   const { data: employees } = useEmployees();
