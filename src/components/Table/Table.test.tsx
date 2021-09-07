fc1f4af28a849c4270c801d789ba255984bd9bea
Ryan Spencer <rspencer@bitovi.com>
2021-09-07 11:38:57 -0500

    test

---
+++ src/components/Table/Table.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { Cell, Header, Row, Table } from ".";
+
+describe("Components/Table", () => {
+  it("renders child Cell text and elements", async () => {
+    render(
+      <Table>
+        <Header>
+          <Cell>Header 1</Cell>
+          <Cell>Header 2</Cell>
+        </Header>
+        <Row>
+          <Cell>Row 1 - Cell 1</Cell>
+          <Cell>
+            <div data-testid="test-div" />
+          </Cell>
+        </Row>
+        <Row>
+          <Cell>Row 2 - Cell 1</Cell>
+          <Cell>Row 2 - Cell 2</Cell>
+        </Row>
+      </Table>,
+    );
+    // Can render headers
+    expect(screen.getByText("Header 1")).toBeInTheDocument();
+    // Can render cells
+    expect(screen.getByText("Row 2 - Cell 2")).toBeInTheDocument();
+    // Can render JSX in cells
+    expect(screen.getByTestId("test-div")).toBeInTheDocument();
+  });
+});
