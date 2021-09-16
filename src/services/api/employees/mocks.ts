d32720fa271a0d05f9b12dd1f0357fbefcf8bcd4
David Nicholas <dnicholas@Davids-MacBook-Pro.local>
2021-09-16 07:06:31 -0700

    Update updater in useRest

--- src/services/api/employees/mocks.ts
+++ src/services/api/employees/mocks.ts
@@ -6,6 +6,27 @@ import { employees } from "./fixtures";
 import { DateString } from "../baseMocks";
 import { createStore, requestCreator } from "../baseMocks";
 
+class DateStringSet {
+  constructor(public value: string) {
+    this.value = value;
+  }
+  // used to convert to a number
+  valueOf(): number {
+    return new Date(this.value).getTime();
+  }
+
+  [Symbol.for("can.serialize")]() {
+    return this.value;
+  }
+}
+
+const DateString = {
+  [Symbol.for("can.new")]: function (v: string): string {
+    return v;
+  },
+  [Symbol.for("can.SetType")]: DateStringSet,
+};
+
 const queryLogic = new QueryLogic<Employee>({
   identity: ["id"],
   keys: {
