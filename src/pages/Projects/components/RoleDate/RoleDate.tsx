0755fb59482b037935e49c7ac9dc4a6e4d5d43b0
Ryan Spencer <rspencer@bitovi.com>
2021-09-16 15:47:00 -0500

    added selects and improved layout

--- src/pages/Projects/components/RoleDate/RoleDate.tsx
+++ src/pages/Projects/components/RoleDate/RoleDate.tsx
@@ -1,3 +1,4 @@
+import { Select } from "../../../../components/Select";
 import type { EstimatedDate } from "../../../../services/api";
 
 import styles from "./RoleDate.module.scss";
@@ -23,19 +24,18 @@ export default function RoleDate({
           }}
         />
       </label>
-      <label>
-        Confidence:
-        <select
-          defaultValue={estimatedDate.confidence}
-          onChange={(e) => {
-            onChange({ ...estimatedDate, confidence: e.currentTarget.value });
-          }}
-        >
-          {Array.from(Array(21).keys()).map((n) => (
-            <option value={`${n * 5}%`} key={title + n}>{`${n * 5}%`}</option>
-          ))}
-        </select>
-      </label>
+      <Select
+        name={`${title.replace(" ", "")}Confidence`}
+        label="Confidence:"
+        value={estimatedDate.confidence}
+        onChange={(value) => {
+          value && onChange({ ...estimatedDate, confidence: value });
+        }}
+        options={Array.from(Array(21).keys()).map((n) => ({
+          value: `${n * 5}%`,
+          label: `${n * 5}%`,
+        }))}
+      />
     </div>
   );
 }
