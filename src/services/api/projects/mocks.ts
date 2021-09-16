3dcff5aae35640816a73d2616807ab102cb5601e
David Nicholas <dnicholas@Davids-MacBook-Pro.local>
2021-09-16 07:20:17 -0700

    Move out shared types and added types to projects

--- src/services/api/projects/mocks.ts
+++ src/services/api/projects/mocks.ts
@@ -1,40 +1,132 @@
-import type { NewProject, Project } from ".";
+import type { NewProject, Project } from "./interfaces";
+import type { MockResponse, QueriableList } from "../shared";
 
 import { rest } from "msw";
+import QueryLogic from "can-query-logic";
+
 import { projects } from "./fixtures";
 
+const queryLogic = new QueryLogic<Project>({
+  identity: ["id"],
+  keys: {
+    id: "string",
+    name: "string",
+    description: "string",
+    roles: "string",
+  },
+});
+
 export default [
-  rest.get("/api/v1/projects", (req, res, ctx) => {
+  rest.get<undefined, MockResponse<Project>, { id: string }>(
+    "/api/v1/projects/:id",
+    (req, res, ctx) => {
+      const id = req.params.id;
+      const project = projects.find((project) => project.id === id);
+
+      if (!project) {
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: `Project ${id} not found.`,
+          }),
+        );
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          data: project,
+        }),
+      );
+    },
+  ),
+
+  rest.put<Partial<Project>, MockResponse<Project>, { id: string }>(
+    "/api/v1/projects/:id",
+    (req, res, ctx) => {
+      const id = req.params.id;
+      const index = projects.findIndex((project) => project.id === id);
+
+      if (index < 0) {
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: `Project ${id} not found.`,
+          }),
+        );
+      }
+
+      const project = projects[index];
+      projects[index] = { ...project, ...req.body, id };
+
+      return res(
+        ctx.status(201),
+        ctx.json({
+          data: project,
+        }),
+      );
+    },
+  ),
+  rest.delete<undefined, MockResponse, { id: string }>(
+    "/api/v1/projects/:id",
+    (req, res, ctx) => {
+      const id = req.params.id;
+      const index = projects.findIndex((project) => project.id === id);
+
+      if (index < 0) {
+        return res(
+          ctx.status(404),
+          ctx.json({
+            error: `Project ${id} not found.`,
+          }),
+        );
+      }
+
+      projects.splice(index, 1);
+
+      return res(ctx.status(200), ctx.json({}));
+    },
+  ),
+  rest.post<NewProject, MockResponse<Project>>(
+    "/api/v1/projects",
+    (req, res, ctx) => {
+      const id = (Math.floor(Math.random() * 1000) + 1).toString();
+      const project = { ...req.body, id };
+
+      projects.push(project);
+      return res(ctx.status(201), ctx.json({ data: project }));
+    },
+  ),
+
+  rest.get<
+    undefined,
+    MockResponse<Project[], { total: number }>,
+    QueriableList<Project>
+  >("/api/v1/projects", (req, res, ctx) => {
+    const { filter, sort, page = 1, count = 25 } = req.params;
+
+    const { data, count: total } = queryLogic.filterMembersAndGetCount(
+      {
+        filter,
+        sort,
+        page: {
+          start: (page - 1) * count,
+          end: page * count - 1,
+        },
+      },
+      {},
+      projects,
+    );
+
     return res(
       ctx.status(200),
       ctx.json({
-        data: projects,
+        data,
+        metadata: {
+          total,
+          pages: Math.ceil(total / count),
+        },
       }),
-    );
-  }),
-
-  rest.post("/api/v1/projects", (req, res, ctx) => {
-    /** @TODO add typing */
-    const project: NewProject = req.body as unknown as Project;
-    const id = (Math.floor(Math.random() * 1000) + 1).toString();
-    projects.push({ ...project, id });
-
-    return res(ctx.status(201), ctx.json({ data: id }));
-  }),
-
-  rest.put("/api/v1/projects/:id", (req, res, ctx) => {
-    const project: Project = req.body as unknown as Project;
-    const index = projects.findIndex((x) => x.id === project.id);
-
-    if (index > -1) {
-      projects[index] = project;
-
-      return res(ctx.status(200), ctx.json({ data: project }));
-    }
-
-    return res(
-      ctx.status(404),
-      ctx.json({ data: "Could not find project with id " + project.id }),
     );
   }),
 ];
