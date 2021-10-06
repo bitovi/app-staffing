e72b0f0c8aaa3692bca705a81f01f07c4f46c5ee
Luca <60432429+luca-bitovi@users.noreply.github.com>
2021-10-06 15:21:25 -0400

    Additional unit tests for List Projects Page components (#99)
    * Adding chai-spies
    
    * Additional testing (#27)
    
    * Updated lock files
    
    * Empty arrow function in Chai spy not needed; was throwing linting error
    
    * Prettier
    
    * Additional tests for `ProjectCard.test.tsx`
    
    * Prettier
    
    * Updated package.json and package-lock.json with `@storybook/addon-docs`

--- src/pages/Projects/components/ListProjects/ListProjects.test.tsx
+++ src/pages/Projects/components/ListProjects/ListProjects.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { expect } from "chai";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { expect, spy } from "chai";
 
 import { MemoryRouter } from "react-router-dom";
 
@@ -7,7 +7,7 @@ import ListProjects from "./ListProjects";
 import { projects } from "../../../../services/api/projects/fixtures";
 
 describe("Components/Layout", () => {
-  it("works", () => {
+  it("renders", () => {
     render(
       <MemoryRouter>
         <ListProjects
@@ -20,5 +20,37 @@ describe("Components/Layout", () => {
 
     const projectContainer = screen.getByText(/Add Project/i);
     expect(projectContainer).to.have.tagName("button");
+  });
+
+  it("calls 'onAddNew' on button click", () => {
+    const onClick = spy();
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <ListProjects
+          projects={projects}
+          onView={(p) => null}
+          onAddNew={onClick}
+        />
+      </MemoryRouter>,
+    );
+
+    fireEvent.click(getByText(/Add Project/i));
+    expect(onClick).to.have.been.called();
+  });
+
+  it("has as many ProjectCards as projects", async () => {
+    const { findAllByText } = render(
+      <MemoryRouter>
+        <ListProjects
+          projects={projects}
+          onView={(p) => null}
+          onAddNew={() => null}
+        />
+      </MemoryRouter>,
+    );
+
+    const projectCards = await findAllByText(/Project:/i);
+    expect(projectCards.length).to.be.equal(projects.length);
   });
 });
