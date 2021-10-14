deee791461ccbf2cee4c59448cae3a671e82d759
akinyele <akinyelethompson@gmail.com>
2021-10-14 10:41:44 -0500

    formatting.

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -8,7 +8,7 @@ export default function Dashboard(): JSX.Element {
   return (
     <Flex flexDirection="column" py={20} px={14}>
       <Flex alignItems="center" marginBottom={10}>
-        <Text textStyle="h1" >Bitovi Staff Management</Text>
+        <Text textStyle="h1">Bitovi Staff Management</Text>
       </Flex>
 
       <ReportTable reportDate={reportDate} />
