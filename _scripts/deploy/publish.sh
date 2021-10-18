d9b06c1485cfb7e0e46503175a3e39aa40331cba
Nosa Ugowe <nugowe@apple.com>
2021-10-18 10:57:38 -0500

    modified git image tagging

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -4,8 +4,8 @@
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
 BRANCH_NAME=$(echo $github_ref | awk -F"  +|/" '{print $5, $NF}')
 REGISTRY_AUTHENTICATION=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL})
-echo "obtaining branch name"
-echo ${BRANCH_NAME}
+
+
 
 if [[ -z "$AWS_ACCOUNT_NO" ]]; then
   echo "check the AWS_ACCOUNT_NO Variable"
@@ -26,17 +26,19 @@ fi
 
  #building the docker image
  docker build -t ${IMAGE_NAME} .
- echo $github_ref
+
 
 
 if [[ BRANCH_NAME == 'main' ]]; then
   IMAGE_TAG="latest"
   echo $REGISTRY_AUTHENTICATION
   docker tag ${IMAGE_NAME} ${REGISTRY_URL}:${IMAGE_TAG}
+  echo ${REGISTRY_URL}:${IMAGE_TAG}
   docker push ${REGISTRY_URL}:${IMAGE_TAG}
 else
   echo $REGISTRY_AUTHENTICATION
-  echo ${REGISTRY_URL}:${BRANCH_NAME}${GITHUB_SHA}
-  docker tag ${IMAGE_NAME}  ${REGISTRY_URL}:${BRANCH_NAME}${GITHUB_SHA}
-  docker push  ${REGISTRY_URL}:${BRANCH_NAME}${GITHUB_SHA}
+  echo ${REGISTRY_URL}:${GITHUB_SHA}
+  docker tag ${IMAGE_NAME}  ${REGISTRY_URL}:${GITHUB_SHA}
+  echo ${REGISTRY_URL}:${GITHUB_SHA}
+  docker push  ${REGISTRY_URL}:${GITHUB_SHA}
 fi
