219ae1a84b009ef71da4a158ee8cca5f22a7af15
Nosa Ugowe <nugowe@apple.com>
2021-10-18 10:50:13 -0500

    troubleshooting docker tag naming conventions

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -4,7 +4,8 @@
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
 BRANCH_NAME=$(echo $github_ref | awk -F"  +|/" '{print $5, $NF}')
 REGISTRY_AUTHENTICATION=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL})
-
+echo "obtaining branch name"
+echo ${BRANCH_NAME}
 
 if [[ -z "$AWS_ACCOUNT_NO" ]]; then
   echo "check the AWS_ACCOUNT_NO Variable"
