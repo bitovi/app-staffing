98cee6e69505e0688f4e8564c7a01c755ced2b37
Nosa Ugowe <nugowe@apple.com>
2021-10-20 12:38:02 -0500

    made modifications to the Registry url

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -4,7 +4,7 @@
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
 BRANCH_NAME=$(echo $GITHUB_REF | awk -F"  +|/" '{print $5, $NF}')
 #REGISTRY_AUTHENTICATION=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL})
-REGISTRY_AUTHENTICATION=docker login -u AWS -p $(aws ecr get-login-password --region ${AWS_DEFAULT_REGION})${REGISTRY_URL}
+REGISTRY_AUTHENTICATION=aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 832297766686.dkr.ecr.us-west-2.amazonaws.com
 
 
 if [[ -z "$AWS_ACCOUNT_NO" ]]; then
