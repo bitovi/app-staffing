bb55f8713b2be301a60f5b82b88fcdcb32e80bdd
Nosa Ugowe <nugowe@apple.com>
2021-10-20 12:48:34 -0500

    made modifications to REGISTRY AUTHENTICATION

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -3,8 +3,8 @@
 
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
 BRANCH_NAME=$(echo $GITHUB_REF | awk -F"  +|/" '{print $5, $NF}')
-#REGISTRY_AUTHENTICATION=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL})
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 832297766686.dkr.ecr.us-west-2.amazonaws.com
+ECR_CRED=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} 
 
+aws ecr --region ${AWS_DEFAULT_REGION} | docker login -u AWS -p ${ECR_CRED} {REGISTRY_URL}
 
 
