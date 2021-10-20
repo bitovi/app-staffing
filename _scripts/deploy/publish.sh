223dd622056256703c0b97bb54c477ba9d80d444
Nosa Ugowe <nugowe@apple.com>
2021-10-20 12:53:00 -0500

    fixed typo on ECR_CRED variable

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -3,8 +3,7 @@
 
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
 BRANCH_NAME=$(echo $GITHUB_REF | awk -F"  +|/" '{print $5, $NF}')
-ECR_CRED=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} 
+ECR_CRED=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION}) 
 
 aws ecr --region ${AWS_DEFAULT_REGION} | docker login -u AWS -p ${ECR_CRED} ${REGISTRY_URL}
-
 
