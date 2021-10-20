7ac71e2600549281ecd8f3efbab821afe16981a4
Nosa Ugowe <nugowe@apple.com>
2021-10-20 12:51:26 -0500

    added $ variable to ecr authentication

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -5,6 +5,6 @@ REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEF
 BRANCH_NAME=$(echo $GITHUB_REF | awk -F"  +|/" '{print $5, $NF}')
 ECR_CRED=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} 
 
-aws ecr --region ${AWS_DEFAULT_REGION} | docker login -u AWS -p ${ECR_CRED} {REGISTRY_URL}
+aws ecr --region ${AWS_DEFAULT_REGION} | docker login -u AWS -p ${ECR_CRED} ${REGISTRY_URL}
 
 
