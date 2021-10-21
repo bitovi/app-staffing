46724bff6be79970d188476774a4276018e350dd
Akinyele Thompson <akinyelethompson@gmail.com>
2021-10-21 08:55:45 -0500

    Dashboard page updates:   - Updated css for report table column heading.   - Updated text for column heading.

--- src/pages/Dashboard/components/ReportTable/ReportTable.tsx
+++ src/pages/Dashboard/components/ReportTable/ReportTable.tsx
@@ -1,29 +1,32 @@
 import { useMemo } from "react";
-import { Flex } from "@chakra-ui/layout";
-import { Spacer, Square, Text } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/layout";
+import { Center, Spacer, Text } from "@chakra-ui/react";
 import {
+  getMonthForWeek,
   getTimescaleData,
   TimescaleData,
   TimescaleType,
 } from "../../../../services/timeReport/timesReport";
-import { format } from "date-fns";
+import { format, setMonth } from "date-fns";
 
 interface IProps {
   reportDate: Date;
 }
 
 export function ReportTable({ reportDate }: IProps): JSX.Element {
-  const timescales: TimescaleData[] = useMemo(
+  const timeFrames: TimescaleData[] = useMemo(
     () => getTimescaleData(reportDate),
     [reportDate],
   );
 
-  const columnHeading: string[] = timescales.map((item) => {
+  const columnHeading: string[] = timeFrames.map((item) => {
     switch (item.type) {
       case TimescaleType.week:
         return format(item.startDate, "MMM do");
       case TimescaleType.month:
-        return format(item.startDate, "MMM yyyy");
+        const monthNum = getMonthForWeek(item.startDate);
+        const monthDate = setMonth(new Date(), monthNum);
+        return format(monthDate, "MMMM");
       case TimescaleType.quarter:
         return `Q${format(item.startDate, "Q yyyy")}`;
       default:
@@ -32,21 +35,42 @@ export function ReportTable({ reportDate }: IProps): JSX.Element {
   });
 
   return (
-    <Flex flex={1}>
-      <Square flex={2} justifyContent={"start"}>
+    <Flex flex={1} height={8} justifyContent={"start"}>
+      <Box flex={2} alignItems={"start"}>
         <Text textStyle="tableHead">Departments</Text>
-      </Square>
+      </Box>
 
       <Spacer />
 
       {/* Table Heading */}
       {columnHeading.map((item, index) => {
         return (
-          <Square flex={1} key={index} justifyContent={"start"}>
-            <Text textStyle="tableHead">{item}</Text>
-          </Square>
+          <Center
+            flex={1}
+            key={index}
+            alignItems={"start"}
+            flexDirection="column"
+          >
+            <Center height={4} flex={1}>
+              <Text textStyle="tableHead">{item}</Text>
+            </Center>
+
+            {/* Sub Heading */}
+            <Center height={4} flex={1}>
+              {timeFrames[index].type === TimescaleType.month && (
+                <Text textStyle="subHead">
+                  {format(timeFrames[index].startDate, "MMM do")}
+                </Text>
+              )}
+            </Center>
+          </Center>
         );
       })}
+
+
+
+
+
     </Flex>
   );
 }
