4d9c38992b8a24bddf746262fb8aa4c649601cec
Nosa Ugowe <nugowe@apple.com>
2021-10-25 11:08:13 -0500

    made changes to the IMAGE_NAME variable

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#defining Registry url variable
+#Defining the Registry url variable
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
 
-#defining Branch name variable
+#Defining the Branch name variable
 BRANCH_NAME=$(echo $GITHUB_REF | awk -F"  +|/" '{print $5, $NF}')
 
-#defining Registry Authentication variablee
+#Defining Registry Authentication variablee
 REGISTRY_AUTHENTICATION=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL})
 
-#defining Default branch variable
+#Defining the Default branch variable
 DEFAULT_BRANCH="main"
 
-#defining IMAGE_TAG variable 
+#Defining the IMAGE_TAG variable 
 IMAGE_TAG="latest"
 
-#building the docker image...
+#Building the docker image...
 docker build -f Dockerfile.dev -t ${IMAGE_NAME} .
 
 if [[ "$BRANCH_NAME" != "$DEFAULT_BRANCH" ]]; then
