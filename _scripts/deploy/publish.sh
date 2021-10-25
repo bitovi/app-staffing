a8b4cc71079e99ba70bc8223fcdb2f7e52aa098f
Nosa Ugowe <nugowe@apple.com>
2021-10-25 10:52:24 -0500

    made changes to the conditional statement in the publish.sh file

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-
+#defining Registry url variable
 REGISTRY_URL=$(printf %s.dkr.ecr.%s.amazonaws.com/%s "$AWS_ACCOUNT_NO" "$AWS_DEFAULT_REGION" "$IMAGE_NAME")
+
+#defining Branch name variable
 BRANCH_NAME=$(echo $GITHUB_REF | awk -F"  +|/" '{print $5, $NF}')
+
+#defining Registry Authentication variablee
 REGISTRY_AUTHENTICATION=$(aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL})
 
+#defining Default branch variable
+DEFAULT_BRANCH="main"
 
-if [[ -z "$AWS_ACCOUNT_NO" ]]; then
-echo "check the AWS_ACCOUNT_NO Variable"
-exit 1
-elif [[ -z "$BRANCH_NAME" ]]; then
-echo "check the BRANCH_NAME
-Variable"
-exit 1 
-elif [[ -z "$REGISTRY_URL" ]]; then
-echo "check the REGISTRY_URL Variable"
-exit 1 
-elif [[ -z "$REGISTRY_AUTHENTICATION" ]]; then
-echo "check the REGISTRY_AUTHENTICATION Variable"
-exit 1 
-else
-echo "Variables defined and ready to publish the image"
-fi
+#defining IMAGE_TAG variable 
+IMAGE_TAG="latest"
 
 #building the docker image...
 docker build -f Dockerfile.dev -t ${IMAGE_NAME} .
 
+if [[ "$BRANCH_NAME" != "$DEFAULT_BRANCH" ]]; then
+  IMAGE_TAG="${GITHUB_SHA}"
+else
 
-if [[ BRANCH_NAME == 'main' ]]; then
-IMAGE_TAG="latest"
 echo $REGISTRY_AUTHENTICATION
 docker tag ${IMAGE_NAME} ${REGISTRY_URL}:${IMAGE_TAG}
+echo "About to push the image......."
 docker push ${REGISTRY_URL}:${IMAGE_TAG}
-else
-echo $REGISTRY_AUTHENTICATION
-echo ${REGISTRY_URL}:${BRANCH_NAME}${GITHUB_SHA}
-docker tag ${IMAGE_NAME} ${REGISTRY_URL}:${GITHUB_SHA}
-docker push ${REGISTRY_URL}:${GITHUB_SHA}
 fi
+
 
 
