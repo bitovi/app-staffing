683526800ea688dde6d7674b227920602cebbf5b
Nosa Ugowe <nugowe@apple.com>
2021-10-28 11:27:16 -0500

    STAF-38 | made changes to the publish.sh script

--- _scripts/deploy/publish.sh
+++ _scripts/deploy/publish.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#defining Image Tag variable
+
+IMAGE_TAG="latest"
+
 #Defining the Image name variable
 IMAGE_NAME=$(echo $GITHUB_REPOSITORY | sed 's/^.*\///')
 
@@ -17,19 +21,19 @@ DEFAULT_BRANCH="main"
 #Building the docker image...
 docker build -f Dockerfile.dev -t ${IMAGE_NAME} .
 
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL}
+
 
 #docker image deploy function
 docker_deploy(){
+aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL}
 docker tag ${IMAGE_NAME} ${REGISTRY_URL}:${IMAGE_TAG}
 echo "About to push the docker image to the ecr repository....."
 docker push ${REGISTRY_URL}:${IMAGE_TAG}
 }
 
 if [[ ${BRANCH_NAME} != ${DEFAULT_BRANCH} ]]; then
-IMAGE_TAG=${GITHUB_SHA}
-docker_deploy
+  IMAGE_TAG=${GITHUB_SHA}
+  
 else
-IMAGE_TAG="latest"
-docker_deploy
-fi
+   echo "BRANCH_NAME is "$BRANCH_NAME". Tagging "$IMAGE_TAG"
+   docker_deploy
