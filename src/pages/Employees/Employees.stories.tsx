676a4ea0b610b7237e49269433495f6c3566212c
Travis Draper <tdraper.dev@gmail.com>
2021-11-03 17:27:02 -0400

    cleaned up a breaking Type issue -- stories not complete

--- src/pages/Employees/Employees.stories.tsx
+++ src/pages/Employees/Employees.stories.tsx
@@ -14,10 +14,20 @@ export const Empty: ComponentStory<typeof Employees> = ({ ...props }) => (
       return {
         isLoading: false,
         employees: [],
-        addEmployee: (employee) => new Promise((resolve) => resolve("")),
-        deleteEmployee: (employeeId) => new Promise((resolve) => resolve()),
-        updateEmployee: (employeeId) => new Promise((resolve) => resolve()),
+        addEmployee: (_employee) => new Promise((resolve):void => resolve('')),
+        deleteEmployee: (_employee) => new Promise((resolve):void => resolve()),
+        updateEmployee: (_employee) => new Promise((resolve):void => resolve()),
         reset: () => undefined,
+      };
+    }}
+    useSkills={() => {
+      return {
+        isLoading: false,
+        employees: [],
+        addEmployee: (_employee: any) => new Promise((resolve):void => resolve('')),
+        //deleteEmployee: (): void => new Promise((resolve):void => resolve()),
+        //updateEmployee: (): void => new Promise((resolve):void => resolve()),
+        //reset: () => undefined,
       };
     }}
   />
