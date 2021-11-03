4d843b0786d8ff2434bd3e67eb1d151c68e32d81
Travis Draper <tdraper.dev@gmail.com>
2021-11-03 17:04:03 -0400

    reverted back to previous changes as my story changes broke the page

--- src/pages/Employees/Employees.stories.tsx
+++ src/pages/Employees/Employees.stories.tsx
@@ -1,17 +1,16 @@
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
 // import { employees } from "../../services/api/employees/fixtures";
-import EmployeesWrapper, { Employees, EmployeePageLoadingLayout } from "./Employees";
-import {useEmployees, useSkills} from '../../services/api'
+import { Employees, EmployeePageLoadingLayout } from "./Employees";
 
 export default {
   title: "Pages/Employees",
-  component: EmployeesWrapper,
+  component: Employees,
 } as ComponentMeta<typeof Employees>;
 
 export const Empty: ComponentStory<typeof Employees> = ({ ...props }) => (
-  <EmployeesWrapper
+  <Employees
     {...props}
-    /*useEmployees={() => {
+    useEmployees={() => {
       return {
         isLoading: false,
         employees: [],
@@ -21,27 +20,24 @@ export const Empty: ComponentStory<typeof Employees> = ({ ...props }) => (
         reset: () => undefined,
       };
     }}
-    useSkills={() => {
-      return {
-        isLoading: false,
-        employees: [],
-        addEmployee: (employee) => new Promise((resolve) => resolve("")),
-        deleteEmployee: (employeeId) => new Promise((resolve) => resolve()),
-        updateEmployee: (employeeId) => new Promise((resolve) => resolve()),
-        reset: () => undefined,
-      };
-    }}*/
-
   />
 );
 
-export const nonEmpty: ComponentStory<typeof Employees> = ({ ...props }) => (
-  <Employees
-    {...props}
-    useEmployees={useEmployees}
-    useSkills={useSkills}
-  />
-);
+// export const nonEmpty: ComponentStory<typeof Employees> = ({ ...props }) => (
+//   <Employees
+//     {...props}
+//     useEmployees={() => {
+//       return {
+//         isLoading: false,
+//         employees: employees,
+//         addEmployee: (employee) => new Promise((resolve) => resolve("")),
+//         deleteEmployee: (employeeId) => new Promise((resolve) => resolve()),
+//         updateEmployee: (employeeId) => new Promise((resolve) => resolve()),
+//         reset: () => undefined,
+//       };
+//     }}
+//   />
+// );
 
 export const Loading: ComponentStory<any> = ({ ...props }) => (
   <EmployeePageLoadingLayout {...props} />
