94bfddb231e3c1a166f41c07c5ca6ad5be55c7a1
Travis Draper <tdraper.dev@gmail.com>
2021-11-10 18:18:36 -0500

    tweaked ServiceError component to make message optional and additional optional styling choices to match figma designs and maintain reusability

--- src/components/ServiceError/ServiceError.test.tsx
+++ src/components/ServiceError/ServiceError.test.tsx
@@ -5,7 +5,8 @@ describe("Components/Service Error", () => {
   it("renders default text with no error inputs", async () => {
     render(<ServiceError />);
     expect(await screen.findByText("Service Error")).toBeInTheDocument();
-    expect(await screen.findByText("Try again later.")).toBeInTheDocument();
+    // ServiceError tweaked to make message field conditionally render
+    // expect(await screen.findByText("Try again later.")).toBeInTheDocument();
   });
 
   it("renders custom error text", async () => {
