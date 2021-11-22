24dd0f9284e68524bbe68f0a8237020f2acbf65d
Travis Draper <tdraper.dev@gmail.com>
2021-11-22 16:47:02 -0500

    updated Employees stories to include grey background and skills fixture data for modal

--- src/pages/Employees/Employees.stories.tsx
+++ src/pages/Employees/Employees.stories.tsx
@@ -1,5 +1,7 @@
+import { Flex, Box } from "@chakra-ui/layout";
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
 import { employeeMockData } from "../../services/api/employees/fixtures";
+import { skills } from "../../services/api/skills/fixtures";
 import { Employees, EmployeePageLoadingLayout } from "./Employees";
 
 export default {
@@ -8,34 +10,46 @@ export default {
 } as ComponentMeta<typeof Employees>;
 
 export const nonEmpty: ComponentStory<typeof Employees> = ({ ...props }) => (
-  <Employees
-    {...props}
-    useEmployees={employeeMockData}
-    useSkills={(): any => {
-      return { skills: null };
-    }}
-  />
+  <Flex height="100%" width="100%" overflow="hidden">
+    <Box backgroundColor="gray.10" flex="1 1" padding="40px" overflow="auto">
+      <Employees
+        {...props}
+        useEmployees={employeeMockData}
+        useSkills={(): any => {
+          return { skills };
+        }}
+      />
+    </Box>
+  </Flex>
 );
 
 export const Empty: ComponentStory<typeof Employees> = ({ ...props }) => (
-  <Employees
-    {...props}
-    useEmployees={() => {
-      return {
-        isLoading: false,
-        employees: [],
-        addEmployee: (employee) => new Promise((resolve) => resolve("")),
-        deleteEmployee: (employeeId) => new Promise((resolve) => resolve()),
-        updateEmployee: (employeeId) => new Promise((resolve) => resolve()),
-        reset: () => undefined,
-      };
-    }}
-    useSkills={(): any => {
-      return { skills: null };
-    }}
-  />
+  <Flex height="100%" width="100%" overflow="hidden">
+    <Box backgroundColor="gray.10" flex="1 1" padding="40px" overflow="auto">
+      <Employees
+        {...props}
+        useEmployees={() => {
+          return {
+            isLoading: false,
+            employees: [],
+            addEmployee: (employee) => new Promise((resolve) => resolve("")),
+            deleteEmployee: (employeeId) => new Promise((resolve) => resolve()),
+            updateEmployee: (employeeId) => new Promise((resolve) => resolve()),
+            reset: () => undefined,
+          };
+        }}
+        useSkills={(): any => {
+          return { skills: null };
+        }}
+      />
+    </Box>
+  </Flex>
 );
 
 export const Loading: ComponentStory<any> = ({ ...props }) => (
-  <EmployeePageLoadingLayout {...props} />
+  <Flex height="100%" width="100%" overflow="hidden">
+    <Box backgroundColor="gray.10" flex="1 1" padding="40px" overflow="auto">
+      <EmployeePageLoadingLayout {...props} />
+    </Box>
+  </Flex>
 );
