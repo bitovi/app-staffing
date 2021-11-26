1db7bcd30cd8190e2c2902c88600afaedb408852
David Nicholas <david_nicholas@apple.com>
2021-11-26 10:39:57 -0800

    udpate Heading and add no only to linter

--- src/components/Typography/Heading/Heading.tsx
+++ src/components/Typography/Heading/Heading.tsx
@@ -1,24 +1,8 @@
 import { Heading as ChakraHeading } from "@chakra-ui/react";
-import { ReactNode } from "react";
 
-interface HeadingProps extends React.ComponentProps<typeof ChakraHeading> {
-  "data-testid"?: string;
-  className?: string;
-  children: ReactNode;
-  variant?: "h1" | "h2";
-}
-
-export function Heading({
-  className,
+export default function Heading({
   variant = "h1",
-  children,
   ...restOfProps
-}: HeadingProps): JSX.Element {
-  return (
-    <ChakraHeading size={variant} {...restOfProps}>
-      {children}
-    </ChakraHeading>
-  );
+}: React.ComponentProps<typeof ChakraHeading>): JSX.Element {
+  return <ChakraHeading size={variant} {...restOfProps} />;
 }
-
-export default Heading;
