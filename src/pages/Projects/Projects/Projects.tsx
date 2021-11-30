d02ba5d00165532dd5f4ccc3b009cfe12483ffcd
Luca <60432429+luca-bitovi@users.noreply.github.com>
2021-11-30 16:53:51 -0500

    Bare-bones addition of heading and header button to projects list page

--- src/pages/Projects/Projects/Projects.tsx
+++ src/pages/Projects/Projects/Projects.tsx
@@ -1,3 +1,5 @@
+import Button from "../../../components/Button";
+import { Flex, Text } from "@chakra-ui/layout";
 import ProjectList from "./components/ProjectList";
 import { useProjects as defaultUseProjects } from "../../../services/api";
 import { LoadingProjectList } from "./components/LoadingProjectList";
@@ -12,6 +14,27 @@ export default function Projects({
 
   return (
     <>
+      <Flex
+        width="full"
+        fontFamily="Arial, Helvetica, sans-serif"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Text textStyle="title" color="gray.700" lineHeight="1">
+          Projects
+        </Text>
+
+        <Button
+          size="lg"
+          variant="primary"
+          onClick={() => null}
+          arialabel="Add Employee"
+        >
+          Add Project
+        </Button>
+      </Flex>
+      
       {isLoading ? (
         <LoadingProjectList />
       ) : error ? (
--- src/theme/fonts/index.ts
+++ src/theme/fonts/index.ts
@@ -30,4 +30,33 @@ export const textStyles = {
     fontSize: "md",
     lineHeight: "md",
   },
+  title: {
+    fontFamily: "Inter",
+    fontWeight: "700",
+    fontSize: "30px",
+    lineHeight: "36px",
+  },
+  table: {
+    title: {
+      fontFamily: "Inter",
+      fontWeight: "bold",
+      fontSize: "12px",
+      lineHeight: "16px",
+      letterSpacing: "0.05em",
+    },
+  },
+  modal: {
+    text: {
+      fontFamily: "Inter",
+      fontWeight: "400",
+      fontSize: "16px",
+      lineHeight: "24px",
+    },
+    button: {
+      fontFamily: "Inter",
+      fontWeight: "600",
+      fontSize: "16px",
+      lineHeight: "24px",
+    },
+  },
 };
