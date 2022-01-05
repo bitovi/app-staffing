a55acfbf9064387c085bf51c92b85711152ded08
Manuel Mujica <manuel.expat@gmail.com>
2022-01-05 13:00:59 -0500

    STAF-90: remove scrollbar from employees page story canvas (#257)

--- src/pages/Employees/Employees.stories.tsx
+++ src/pages/Employees/Employees.stories.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Flex, Box } from "@chakra-ui/layout";
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
 import { employeeMockData } from "../../services/api/employees/fixtures";
@@ -11,19 +12,7 @@ export default {
 
 const backgroundColor = "gray.10";
 
-export const nonEmpty: ComponentStory<typeof Employees> = ({ ...props }) => (
-  <Flex height="100%" width="100%" overflow="hidden">
-    <Box backgroundColor={backgroundColor} flex="1 1" padding="40px">
-      <Employees
-        {...props}
-        useEmployees={employeeMockData}
-        useSkills={() => {
-          return { skills, isLoading: false };
-        }}
-      />
-    </Box>
-  </Flex>
-);
+export const nonEmpty: ComponentStory<typeof Employees> = NonEmptyEmployeesPage;
 
 export const Empty: ComponentStory<typeof Employees> = ({ ...props }) => (
   <Flex height="100%" width="100%" overflow="hidden">
@@ -55,10 +44,36 @@ export const Empty: ComponentStory<typeof Employees> = ({ ...props }) => (
   </Flex>
 );
 
-export const Loading: ComponentStory<any> = ({ ...props }) => (
+export const Loading: ComponentStory<typeof Flex> = ({ ...props }) => (
   <Flex height="100%" width="100%" overflow="hidden">
     <Box backgroundColor={backgroundColor} flex="1 1" padding="40px">
       <EmployeePageLoadingLayout {...props} />
     </Box>
   </Flex>
 );
+
+function NonEmptyEmployeesPage({ ...props }) {
+  // STAF-90: Prevent an "extra vertical scrollbar" in the storybook canvas to
+  // show up; the non-empty employees page adds a scrollable table section and
+  // having two vertical scrollbars is undesirable.
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.removeProperty("overflow");
+    };
+  }, []);
+
+  return (
+    <Flex height="100%" width="100%" overflow="hidden">
+      <Box backgroundColor={backgroundColor} flex="1 1" padding="40px">
+        <Employees
+          {...props}
+          useEmployees={employeeMockData}
+          useSkills={() => {
+            return { skills, isLoading: false };
+          }}
+        />
+      </Box>
+    </Flex>
+  );
+}
