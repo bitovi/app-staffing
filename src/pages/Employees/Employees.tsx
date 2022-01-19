a155a59ae2dcb3ddb9f7e459f3b10c50dd43fb54
Youssef <youssef@bitovi.com>
2022-01-19 09:53:39 -0500

    staf-80: add breadcrumbs to employees page

--- src/pages/Employees/Employees.tsx
+++ src/pages/Employees/Employees.tsx
@@ -1,5 +1,7 @@
 import { Suspense, useState } from "react";
 import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 import {
   Employee,
   useEmployees as useEmployeesDefault,
@@ -81,6 +83,26 @@ export function Employees({
         onSave={addNewEmployee}
         skills={skills}
       />
+
+      <Breadcrumb
+        spacing="8px"
+        marginBottom="16px"
+        fontSize="14px"
+        color="gray.500"
+        separator={<ChevronRightIcon />}
+      >
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/" data-testid="homeBreadcrumb">
+            Home
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem isCurrentPage color="gray.800">
+          <BreadcrumbLink data-testid="employeesBreadcrumb" cursor="default" _hover={{ textDecoration: 'none' }}>
+            Team Members
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
 
       <Flex
         width="full"
