6f2afc128356494589cc2cfd076e1d5ad05454f9
Ryan Spencer <rspencer@bitovi.com>
2022-02-08 09:28:36 -0600

    demo change

--- src/pages/Projects/Projects/components/ProjectsHeader/ProjectsHeader.tsx
+++ src/pages/Projects/Projects/components/ProjectsHeader/ProjectsHeader.tsx
@@ -69,6 +69,7 @@ export default function ProjectsHeader({
               arialabel="Add Project"
               onClick={onOpen}
               data-testid="addProjectButton"
+              style={{ padding: ' 10px 30px' }}
             >
               Add Project
             </Button>
