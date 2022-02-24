ec152b53b585d7e9f6f6a66c53ee2275bc4ed6f9
Youssef <youssef@bitovi.com>
2022-02-24 15:12:33 -0500

    staf-53: list actual skills in dashboard

--- src/pages/Dashboard/components/ReportTable/ReportTable.tsx
+++ src/pages/Dashboard/components/ReportTable/ReportTable.tsx
@@ -4,6 +4,7 @@ import { VStack } from "@chakra-ui/react";
 import { useProjection } from "../../../../services/projection";
 import TableRow from "./TableRow";
 import TableHeader from "./TableHeader";
+import { useSkills } from "../../../../services/api";
 
 interface ReportTableProps {
   date?: Date;
@@ -12,13 +13,14 @@ interface ReportTableProps {
 export function ReportTable({
   date = new Date(),
 }: ReportTableProps): JSX.Element {
-  const { timeline, skills } = useProjection(date);
+  const availableSkills = useSkills();
+  const { timeline, skills } = useProjection(date, availableSkills);
 
   return (
     <Flex flexDirection="column">
       <TableHeader timeline={timeline} columnLabel={"DEPARTMENT"}></TableHeader>
 
-      <VStack spacing={4} align="stretch">
+      <VStack spacing={4} align="stretch" maxHeight="73vh" overflowY="auto">
         {skills.map(({ skill, projections }) => (
           <TableRow key={skill.id} skill={skill} projections={projections} />
         ))}
--- src/services/projection/projections/projections.ts
+++ src/services/projection/projections/projections.ts
@@ -14,55 +14,25 @@ export interface ProjectionGroup {
 
 export default function getProjections(
   timeline: TimelineRange[],
+  availableSkills: Skill[],
 ): ProjectionGroup[] {
-  return [
-    {
-      skill: { id: "101", name: "Angular" },
+  const getRandomInt = (): number => {
+    return Math.floor(Math.random() * 4);
+  };
+
+  const getRandomAction = (): string => {
+    const actions = ["OK", "Hire", "Assign", "Sell"];
+    return actions[getRandomInt()];
+  };
+
+  return availableSkills.map((skill) => {
+    return {
+      skill,
       projections: new Array<Projection>(timeline.length).fill({
-        needed: 0,
-        bench: 0,
-        action: "OK",
+        needed: getRandomInt(),
+        bench: getRandomInt(),
+        action: getRandomAction(),
       }),
-    },
-    {
-      skill: { id: "104", name: "React" },
-      projections: new Array<Projection>(timeline.length).fill({
-        needed: 3,
-        bench: 1,
-        action: "Hire",
-      }),
-    },
-    {
-      skill: { id: "102", name: "DevOps" },
-      projections: new Array<Projection>(timeline.length).fill({
-        needed: 2,
-        bench: 0,
-        action: "Hire",
-      }),
-    },
-    {
-      skill: { id: "103", name: "Node" },
-      projections: new Array<Projection>(timeline.length).fill({
-        needed: 3,
-        bench: 3,
-        action: "Assign",
-      }),
-    },
-    {
-      skill: { id: "105", name: "Product" },
-      projections: new Array<Projection>(timeline.length).fill({
-        needed: 0,
-        bench: 2,
-        action: "Sell",
-      }),
-    },
-    {
-      skill: { id: "106", name: "Project Management" },
-      projections: new Array<Projection>(timeline.length).fill({
-        needed: 1,
-        bench: 3,
-        action: "Sell",
-      }),
-    },
-  ];
+    };
+  });
 }
