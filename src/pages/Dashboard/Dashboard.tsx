446537e1481f688af25da70b65945bc6a58bfbc8
Youssef <youssef@bitovi.com>
2022-02-24 16:09:15 -0500

    fix test and add suspense to dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,35 @@
 import ReportTable from "./components/ReportTable";
 import { Flex } from "@chakra-ui/layout";
 import Heading from "../../components/Typography/Heading";
+import { Suspense } from "react";
 
-export default function Dashboard(): JSX.Element {
+export function DashboardLoadingLayout(): JSX.Element {
   return (
     <Flex flexDirection="column" py={20} px={14}>
       <Flex alignItems="center" marginBottom={10}>
         <Heading variant="h1">Bitovi Staff Management</Heading>
       </Flex>
+    </Flex>
+  );
+}
+
+export function Dashboard(): JSX.Element {
+  return (
+    <Flex flexDirection="column" py={20} px={14}>
+      <Flex alignItems="center" marginBottom={10}>
+        <Heading variant="h1" data-testid="dashboardTitle">
+          Bitovi Staff Management
+        </Heading>
+      </Flex>
       <ReportTable />
     </Flex>
+  );
+}
+
+export default function DashboardWrapper(): JSX.Element {
+  return (
+    <Suspense fallback={<DashboardLoadingLayout />}>
+      <Dashboard />
+    </Suspense>
   );
 }
