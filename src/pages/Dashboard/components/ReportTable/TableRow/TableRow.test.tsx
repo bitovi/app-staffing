fd7aa750f10b6a9903b3bdf49b53b4cec87bddfc
Zagros Maroofzadeh <zagros@Zagross-MBP.ht.home>
2022-05-05 19:30:58 -0400

    added prettier formatting

--- src/pages/Dashboard/components/ReportTable/TableRow/TableRow.test.tsx
+++ src/pages/Dashboard/components/ReportTable/TableRow/TableRow.test.tsx
@@ -8,15 +8,18 @@ import theme from "../../../../../theme";
 import TableRow from "./TableRow";
 describe("Pages/Dashboard/ReportTable/TableRow", () => {
   it("renders the first TableRow component", async () => {
-      const {result} = renderHook(()=>useProjection(new Date(), skills));
+    const { result } = renderHook(() => useProjection(new Date(), skills));
     render(
-        <MemoryRouter>
-            <StylesProvider value={theme}>
-                <table>
-                    <TableRow skill={skills[0]} projections={result.current.skillsWithProjection[0].projections} />
-                </table>
-            </StylesProvider>
-        </MemoryRouter>
+      <MemoryRouter>
+        <StylesProvider value={theme}>
+          <table>
+            <TableRow
+              skill={skills[0]}
+              projections={result.current.skillsWithProjection[0].projections}
+            />
+          </table>
+        </StylesProvider>
+      </MemoryRouter>,
     );
     expect(await screen.findByText(skills[0].name)).toBeInTheDocument();
   });
