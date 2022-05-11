6f72942e432d905be5ce3a22483aa5f2730cddbf
Zagros Maroofzadeh <zagros@Zagross-MBP.ht.home>
2022-05-11 15:15:09 -0400

    added story book case 8 for issue in ticket

--- src/services/projection/projections/projectionStories/projections.stories.tsx
+++ src/services/projection/projections/projectionStories/projections.stories.tsx
@@ -584,6 +584,68 @@ NeededUseCase7.parameters = {
   controls: { hideNoControlsWarning: true },
 };
 
+export const NeededUseCase8: ComponentStory<typeof ProjectionsContainer> =
+  () => {
+    const dashboardStart = new Date(2018, 0, 1);
+    const skill = { id: "1001", name: "React" };
+    const role = {
+      id: "1",
+      startDate: new Date(2018, 0, 1),
+      startConfidence: 1,
+      endDate: new Date(2018, 0, 10),
+      endConfidence: 0.8,
+      project: projects[0],
+      skills: [skill],
+    };
+
+    const employee = {
+      id: "20",
+      name: "John Doe",
+      skills: [skill],
+    };
+    const roles = [
+      {
+        ...role,
+        assignments: [
+          {
+            id: "2",
+            employee,
+            role,
+            startDate: new Date(2018, 0, 1),
+            endDate: new Date(2018, 0, 10),
+          },
+        ],
+      },
+    ];
+
+    const skillsWithRoles = [
+      {
+        ...skill,
+        roles,
+      },
+    ];
+
+    const { skillsWithProjection } = useProjection(
+      dashboardStart,
+      skillsWithRoles,
+    );
+
+    const projections = skillsWithProjection[0].projections;
+    return (
+      <ProjectionsContainer
+        title="Role ends in the middle of a week"
+        dashboardStart={dashboardStart}
+        roles={roles}
+        skill={skill}
+        projections={projections}
+      />
+    );
+  };
+
+NeededUseCase8.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
+
 export const BenchUseCase1: ComponentStory<typeof ProjectionsContainer> =
   () => {
     const dashboardStart = new Date(2018, 0, 1);
