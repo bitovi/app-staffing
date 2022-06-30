494138384c4eca2569d6e7ef2b9543e61e0d7849
Christina Labay <christina@bitovi.com>
2022-06-30 14:09:56 -0500

    Staf 402 (#331)
    * Add error boundries around routes
    
    Co-authored-by: Agustin Cassani <agustin@bitovi.com>

---
+++ src/components/ErrorBoundary/ErrorBondary.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+
+import ErrorBoundary from "./ErrorBoundary";
+
+describe("Components/ErrorBoundary", () => {
+  it("should show error message", () => {
+    const BuggyComponent = () => {
+      throw new Error("This component is throwing an error.");
+    };
+
+    render(
+      <ErrorBoundary>
+        <BuggyComponent />
+      </ErrorBoundary>,
+    );
+
+    const errorMessage = screen.getByText(
+      "This component is throwing an error.",
+    );
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("should show default error message", () => {
+    const BuggyComponent = () => {
+      throw new Error();
+    };
+
+    render(
+      <ErrorBoundary>
+        <BuggyComponent />
+      </ErrorBoundary>,
+    );
+
+    const errorMessage = screen.getByText("Sorry... there was an error.");
+    expect(errorMessage).toBeInTheDocument();
+  });
+});
