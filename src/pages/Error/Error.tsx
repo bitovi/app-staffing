5cf4fa42798c585f5f9d8580c03fc0350c4d0415
Agustin Cassani <agustin@bitovi.com>
2022-06-30 14:49:03 -0500

    404 svg in place.

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,7 +1,32 @@
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { PageNotFound } from "../assets/PageNotFound";
+import { useHistory } from "react-router-dom";
+
 export default function Error(): JSX.Element {
+  const history = useHistory();
+
+  function onClick() {
+    history.push("/");
+  }
+
   return (
-    <div>
-      <h1>Error: Not Found</h1>
-    </div>
+    <Flex
+      alignItems="center"
+      flexDirection="column"
+      justifyContent="center"
+      padding={100}
+    >
+      <PageNotFound />
+      <Heading as="h2" padding={10} size="2xl">
+        404 PAGE NOT FOUND
+      </Heading>
+      <Text fontSize={16} fontWeight={400} paddingBottom={10}>
+        Check that you typed the address correctly, go back to your previous
+        page or click a selection from the menu on the left.
+      </Text>
+      <Button colorScheme="teal" onClick={onClick} size="md">
+        Return home
+      </Button>
+    </Flex>
   );
 }
