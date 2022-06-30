d34458a7b3f392e80ae4852aee8515f40810dd38
Akinyele Thompson <akinyelethompson@gmail.com>
2022-06-30 11:41:45 -0500

    - updated DataTimelineHeader test to ensure that date ranges are rendered.

--- src/components/DataTable/DataTimelineHeader/DataTimelineHeader.test.tsx
+++ src/components/DataTable/DataTimelineHeader/DataTimelineHeader.test.tsx
@@ -6,17 +6,48 @@ describe("Components/DataTimelineHeader", function () {
   it("works", async () => {
     const timelines: TimelineRange[] = [
       {
-        type: "type",
-        title: "title",
-        startDate: new Date(1, 2, 2022),
-        endDate: new Date(1, 3, 2022),
+        startDate: new Date("2022-06-27T16:35:40.727Z"),
+        endDate: new Date("2022-07-04T16:35:40.726Z"),
+        type: "week",
+        title: "Jun 27th",
+      },
+      {
+        startDate: new Date("2022-07-04T16:35:40.727Z"),
+        endDate: new Date("2022-07-11T16:35:40.726Z"),
+        type: "week",
+        title: "Jul 4th",
+      },
+      {
+        startDate: new Date("2022-07-11T16:35:40.727Z"),
+        endDate: new Date("2022-07-18T16:35:40.726Z"),
+        type: "week",
+        title: "Jul 11th",
+      },
+
+      {
+        startDate: new Date("2023-01-02T06:00:00.000Z"),
+        endDate: new Date("2023-04-03T04:59:59.999Z"),
+        type: "quarter",
+        title: "Q1 2023",
       },
     ];
     const heading = "Department";
 
     render(<DataTimelineHeader timeline={timelines} heading={heading} />);
 
-    const title = screen.getByText(heading);
-    expect(title).toBeInTheDocument();
+    const headingDom = screen.getByText(heading);
+    expect(headingDom).toBeInTheDocument();
+
+    const titleDom = screen.getByText(timelines[0].title);
+    expect(titleDom).toBeInTheDocument();
+
+    const titleDom1 = screen.getByText(timelines[1].title);
+    expect(titleDom1).toBeInTheDocument();
+
+    const titleDom2 = screen.getByText(timelines[2].title);
+    expect(titleDom2).toBeInTheDocument();
+
+    const titleDom3 = screen.getByText(timelines[3].title);
+    expect(titleDom3).toBeInTheDocument();
   });
 });
