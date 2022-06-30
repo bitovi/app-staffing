6443fc1d1e9fe5961bbcfb5c76d8381038452370
Youssef <youssef@bitovi.com>
2022-06-30 09:42:09 -0500

    staf-398: add suspense to skills page

---
+++ src/pages/Skills/Skills.test.tsx
@@ -0,0 +1,61 @@
+import { cleanup, render } from "@testing-library/react";
+import { skills } from "../../mocks/fixtures";
+import Skills from "./Skills";
+
+describe("Pages/Skills", () => {
+  describe("Skills fallback when loading", () => {
+    afterEach(async () => {
+      cleanup();
+    });
+
+    const useSkillsMocked = () => {
+      throw new Promise(() => {});
+    };
+
+    it("renders table headers", async () => {
+      const { findByText } = render(<Skills useSkills={useSkillsMocked} />);
+      const tableHeaderName = await findByText(/Skill name/i);
+      const tableHeaderAction = await findByText(/Actions/i);
+      expect(tableHeaderName).toBeInTheDocument();
+      expect(tableHeaderAction).toBeInTheDocument();
+    });
+
+    it("renders Add Skill button", async () => {
+      const { findByText } = render(<Skills />);
+      const addSkillButton = await findByText("Add Skill");
+      expect(addSkillButton).toBeInTheDocument();
+    });
+
+    it("renders skeleton rows", async () => {
+      const { getAllByTestId } = render(<Skills />);
+      const skeletonRows = await getAllByTestId("skeleton-row");
+      expect(skeletonRows[0]).toBeInTheDocument();
+    });
+
+    // Should test that clicking the button triggers the action when action is implemented
+  });
+
+  describe("Skills with data", () => {
+    afterEach(async () => {
+      cleanup();
+    });
+
+    const useSkillsMocked = () => {
+      return skills;
+    };
+
+    it("renders Add Skill button", async () => {
+      const { findByText } = render(<Skills useSkills={useSkillsMocked} />);
+      const addSkillButton = await findByText("Add Skill");
+      expect(addSkillButton).toBeInTheDocument();
+    });
+
+    it("renders Edit Skill buttons", async () => {
+      const { findAllByRole } = render(<Skills useSkills={useSkillsMocked} />);
+      const editIcons = await findAllByRole("button", {
+        name: "Edit Skill",
+      });
+      expect(editIcons[0]).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/pages/Skills/components/SkillsTable/SkillsTableBody/SkillsTableBody.tsx
@@ -0,0 +1,35 @@
+import { Tbody, Tr } from "@chakra-ui/react";
+import { Skill } from "../../../../../services/api";
+import SkillCard from "./SkillCard/SkillCard";
+
+const SkillsTableBody = ({ skills }: { skills: Skill[] }) => {
+  return (
+    <Tbody>
+      {skills.map((skill, index) => (
+        <SkillTableRow
+          key={skill.id}
+          skill={skill}
+          lastChild={skills.length - 1 === index}
+        />
+      ))}
+    </Tbody>
+  );
+};
+
+function SkillTableRow({
+  skill,
+  lastChild = false,
+}: {
+  skill: Skill;
+  lastChild: boolean;
+}) {
+  return (
+    <>
+      <SkillCard skill={skill} />
+      {/* add space between rows */}
+      {!lastChild && <Tr height={4} />}
+    </>
+  );
+}
+
+export default SkillsTableBody;
