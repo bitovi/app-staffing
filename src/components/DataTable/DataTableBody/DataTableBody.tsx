a722a5445d1df5a8b9e874419c684e9e03fa455c
Zagros Maroofzadeh <zagros@Zagross-MBP.ht.home>
2022-07-18 10:16:46 -0400

    switching branch, currently works but code not cleaned up

--- src/components/DataTable/DataTableBody/DataTableBody.tsx
+++ src/components/DataTable/DataTableBody/DataTableBody.tsx
@@ -3,12 +3,12 @@ import { Text } from "@chakra-ui/layout";
 import { Link, Box, Flex, Tooltip } from "@chakra-ui/react";
 import { skillBackgrounds } from "../../../pages/Dashboard/components/ReportTable/TableRow/TableRow";
 import Badge from "../../Badge";
-import { Project, Role, Skill } from "../../../services/api";
+import { Assignment, Project, Role } from "../../../services/api";
 import { TimelineRange } from "../../../services/projection";
 import { colors } from "../../../theme/colors";
 import { formatDateToUTC } from "../../../services/helpers/utcdate";
 import { HoverInfo } from "../../../pages/Projects/Projects/components/ProjectHoverInfo/ProjectHoverInfo";
-
+import { v4 as uuidv4 } from "uuid";
 interface PropjectCardProps {
   project: Project;
   timeline?: TimelineRange[];
@@ -49,41 +49,43 @@ const DataTableBody = ({
         {project?.roles?.map((role) => (
           <Box key={role.id}>
             {role?.skills?.map((skill) => (
-              <Tooltip
+              <Flex
                 key={skill.id}
-                minWidth="400px"
-                height="fit-content"
-                hasArrow
-                placement="top"
-                label={<HoverInfo role={role} skill={skill} />}
-                aria-label="project start and end tooltip"
+                alignItems="center"
+                borderBottom="1px solid rgba(0, 0, 0, 0.04)"
+                minHeight="50px"
               >
                 <Flex
                   alignItems="center"
-                  borderBottom="1px solid rgba(0, 0, 0, 0.04)"
-                  minHeight="50px"
+                  alignSelf="stretch"
+                  flex="0 1 150px"
+                  justify="center"
+                  padding="0 16px"
                 >
-                  <Flex
-                    alignItems="center"
-                    alignSelf="stretch"
-                    flex="0 1 150px"
-                    justify="center"
-                    padding="0 16px"
+                  <Badge
+                    background={skillBackgrounds[skill.name]}
+                    display="flex"
+                    isTruncated={false}
+                    maxWidth="100px"
+                    size="sm"
+                    textAlign="center"
+                    whiteSpace="break-spaces"
                   >
-                    <Badge
-                      background={skillBackgrounds[skill.name]}
-                      display="flex"
-                      isTruncated={false}
-                      maxWidth="100px"
-                      size="sm"
-                      textAlign="center"
-                      whiteSpace="break-spaces"
+                    {skill.name}
+                  </Badge>
+                </Flex>
+
+                {timeline.map((item: TimelineRange, index: number) => {
+                  return (
+                    <Tooltip
+                      minWidth="400px"
+                      height="fit-content"
+                      hasArrow
+                      key={`${!!item}=${index}`}
+                      placement="top"
+                      label={<HoverInfo role={role} skill={skill} />}
+                      aria-label="project start and end tooltip"
                     >
-                      {skill.name}
-                    </Badge>
-                  </Flex>
-                  {timeline.map((item: TimelineRange, index: number) => {
-                    return (
                       <Box
                         textAlign="center"
                         alignSelf="stretch"
@@ -91,16 +93,28 @@ const DataTableBody = ({
                           index % 2 === 0 ? "rgba(0,0,0,.04)" : "transparent"
                         }
                         flex="1"
-                        key={`${!!item}=${index}`}
                       >
-                        <Flex width="100%" height="100%">
-                          {getGanttCell(role, timeline, index, skill)}
+                        <Flex marginTop="14px" flexDirection="column">
+                          {getGanttCell([role], timeline, index)}
+                        </Flex>
+                        <Flex title="role/assignments" flexDirection="column">
+                          {role.assignments &&
+                            role.assignments.length > 0 &&
+                            groupAssignments(role.assignments, timeline).map(
+                              (assignments: Assignment[]) => {
+                                return getGanttCell(
+                                  assignments,
+                                  timeline,
+                                  index,
+                                );
+                              },
+                            )}
                         </Flex>
                       </Box>
-                    );
-                  })}
-                </Flex>
-              </Tooltip>
+                    </Tooltip>
+                  );
+                })}
+              </Flex>
             ))}
           </Box>
         ))}
@@ -112,7 +126,7 @@ const DataTableBody = ({
 export default DataTableBody;
 
 function isRoleInTimeline(
-  role: Role,
+  role: Role | Assignment,
   timeline: TimelineRange[],
   index: number,
 ): boolean {
@@ -136,55 +150,109 @@ function isRoleInTimeline(
   return false;
 }
 function getGanttCell(
-  role: Role,
+  roleAssignments: Role[] | Assignment[],
   timeline: TimelineRange[],
   index: number,
-  skill: Skill,
 ) {
   let color = "transparent";
-  const borderRadius = ["0", "0", "0", "0"];
+  let borderRadius = ["0", "0", "0", "0"];
   let width = 100;
   let rightAlign = true; // if true then align the cell right (the starting gantt cell in this case)
-  if (isRoleInTimeline(role, timeline, index)) {
-    color = getStartConfidenceColor(role.startConfidence);
-    if (index > 0) {
-      if (!isRoleInTimeline(role, timeline, index - 1)) {
-        borderRadius[0] = "8px";
-        borderRadius[3] = "8px"; //farthest to the left
-        width = calculateGanttCellWidth(role, timeline, index, false);
-      } else if (index === timeline.length - 1) {
-        rightAlign = false; //farthest to the right
-        width = calculateGanttCellWidth(role, timeline, index, true);
-      }
-    } else {
-      // first item in timeline (farthest to the left)
-      width = calculateGanttCellWidth(role, timeline, index, false);
-      if (width < 100) {
-        borderRadius[0] = "8px";
-        borderRadius[3] = "8px";
-      }
+  let skipRow = false;
+  const boxes: JSX.Element[] = [];
+  for (let i = 0; i < roleAssignments.length; i++) {
+    let roleAssignment: Role | Assignment = roleAssignments[i];
+    skipRow = false;
+    if (!showInTimeline(roleAssignment, timeline)) {
+      skipRow = true;
+      continue;
     }
-    if (index < timeline.length - 1) {
-      if (!isRoleInTimeline(role, timeline, index + 1)) {
-        // then last cell shown in timeline (farthest to the right)
-        borderRadius[1] = "8px";
-        borderRadius[2] = "8px";
-        rightAlign = false;
-        width = calculateGanttCellWidth(role, timeline, index, true);
+    color = "transparent";
+    borderRadius = ["0", "0", "0", "0"];
+    width = 100;
+    rightAlign = true; // if true then align the cell right (the starting gantt cell in this case)
+    if (isRoleInTimeline(roleAssignment, timeline, index)) {
+      if ("startConfidence" in roleAssignment) {
+        roleAssignment = roleAssignment as Role;
+        color = getStartConfidenceColor(roleAssignment.startConfidence);
+      } else {
+        color = "grey";
       }
+
+      if (index > 0) {
+        if (!isRoleInTimeline(roleAssignment, timeline, index - 1)) {
+          borderRadius[0] = "8px";
+          borderRadius[3] = "8px"; //farthest to the left
+          width = calculateGanttCellWidth(
+            roleAssignment,
+            timeline,
+            index,
+            false,
+          );
+        } else if (index === timeline.length - 1) {
+          rightAlign = false; //farthest to the right
+          width = calculateGanttCellWidth(
+            roleAssignment,
+            timeline,
+            index,
+            true,
+          );
+        }
+      } else {
+        // first item in timeline (farthest to the left)
+        width = calculateGanttCellWidth(roleAssignment, timeline, index, false);
+        if (width < 100) {
+          borderRadius[0] = "8px";
+          borderRadius[3] = "8px";
+        }
+      }
+      if (index < timeline.length - 1) {
+        if (!isRoleInTimeline(roleAssignment, timeline, index + 1)) {
+          // then last cell shown in timeline (farthest to the right)
+          borderRadius[1] = "8px";
+          borderRadius[2] = "8px";
+          rightAlign = false;
+          width = calculateGanttCellWidth(
+            roleAssignment,
+            timeline,
+            index,
+            true,
+          );
+        }
+      }
+      boxes.push(
+        <Box
+          minHeight="18px"
+          margin="auto"
+          marginLeft={rightAlign ? "auto" : 0}
+          marginRight={rightAlign ? 0 : "auto"}
+          borderRadius={`${borderRadius[0]} ${borderRadius[1]} ${borderRadius[2]} ${borderRadius[3]}`}
+          width={`${width}%`}
+          opacity={0.7}
+          backgroundColor={color}
+        />,
+      );
     }
   }
+  if (!boxes.length && !skipRow) {
+    boxes.push(
+      <Box
+        minHeight="18px"
+        margin="auto"
+        marginLeft={rightAlign ? "auto" : 0}
+        marginRight={rightAlign ? 0 : "auto"}
+        borderRadius={`${borderRadius[0]} ${borderRadius[1]} ${borderRadius[2]} ${borderRadius[3]}`}
+        width={`${width}%`}
+        opacity={0.7}
+        backgroundColor={color}
+      />,
+    );
+  }
+  const id = uuidv4();
   return (
-    <Box
-      minHeight="18px"
-      margin="auto"
-      marginLeft={rightAlign ? "auto" : 0}
-      marginRight={rightAlign ? 0 : "auto"}
-      borderRadius={`${borderRadius[0]} ${borderRadius[1]} ${borderRadius[2]} ${borderRadius[3]}`}
-      width={`${width}%`}
-      opacity={0.7}
-      backgroundColor={color}
-    />
+    <Flex key={`${id}-${boxes.length}`} marginBottom="8px">
+      {boxes.map((box) => box)}
+    </Flex>
   );
 }
 
@@ -219,7 +287,7 @@ function getStartConfidenceColor(startConfidence: number) {
 }
 
 function calculateGanttCellWidth(
-  role: Role,
+  role: Role | Assignment,
   timeline: TimelineRange[],
   index: number,
   useEndDate: boolean,
@@ -242,4 +310,59 @@ function calculateGanttCellWidth(
     width = 100;
   }
   return width;
+}
+
+function groupAssignments(
+  assignments: Assignment[],
+  timeline: TimelineRange[],
+) {
+  // loop through assignment
+  // if start date is greater than end date of previous group together
+  assignments = assignments.filter((assignment: Assignment) => {
+    return showInTimeline(assignment, timeline);
+  });
+  assignments.sort(
+    (a: Assignment, b: Assignment) =>
+      new Date(a.startDate).getTime() - new Date(b.startDate).getTime(),
+  );
+  //sort by start date
+  const groupedAssignments: [Assignment[]] = [[assignments[0]]];
+  for (let i = 1; i < assignments.length; i++) {
+    const assignment = assignments[i];
+    groupedAssignments[groupedAssignments.length - 1].sort(
+      (a: Assignment, b: Assignment) =>
+        new Date(a.startDate).getTime() - new Date(b.startDate).getTime(),
+    );
+    let doneLoop = false;
+    for (let j = 0; j < groupedAssignments.length; j++) {
+      const lastAssignment =
+        groupedAssignments[j][groupedAssignments[j].length - 1];
+      if (lastAssignment && lastAssignment.endDate) {
+        const curEnddate: Date = new Date(lastAssignment.endDate);
+        if (new Date(assignment.startDate).getTime() > curEnddate.getTime()) {
+          groupedAssignments[j].push(assignment);
+          doneLoop = true;
+          continue;
+        }
+      }
+    }
+    if (doneLoop) {
+      continue;
+    }
+    groupedAssignments.push([assignment]);
+  }
+  return groupedAssignments;
+}
+
+function showInTimeline(
+  roleAssignment: Role | Assignment,
+  timeline: TimelineRange[],
+) {
+  if (!roleAssignment) {
+    return false;
+  }
+  return (
+    new Date(roleAssignment.startDate).getTime() <
+    timeline[timeline.length - 1].endDate.getTime()
+  );
 }
