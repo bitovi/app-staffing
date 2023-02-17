2a7d67f4e002d554ca97f0f2a931c8308b05d331
Arthur Pankiewicz <arthur@bitovi.com>
2023-02-17 11:19:19 -0800

    add storybook msw, stories for skills & employees

--- src/scaffold/app/Skills/Skills.stories.tsx
+++ src/scaffold/app/Skills/Skills.stories.tsx
@@ -3,38 +3,38 @@ import type { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Flex, Box } from "@chakra-ui/layout";
 
-import Employees from "./Employees";
+import Skills from "./Skills";
 import { BrowserRouter } from "react-router-dom";
 
 export default {
-  title: "Pages/Employees/List",
-  component: Employees,
-} as ComponentMeta<typeof Employees>;
+  title: "Pages/Skills/List",
+  component: Skills,
+} as ComponentMeta<typeof Skills>;
 
 const backgroundColor = "gray.10";
 
 const mswHandlers = [
-  rest.post(/employees/, (_, res, ctx) => {
+  rest.post(/skills/, (_, res, ctx) => {
     return res(
       ctx.status(201),
-      ctx.json({ data: { attributes: { name: "msw user" } } }),
+      ctx.json({ data: { attributes: { name: "msw skill" } } }),
     );
   }),
-  rest.patch(/employees/, (_, res, ctx) => {
+  rest.patch(/skills/, (_, res, ctx) => {
     return res(
       ctx.status(201),
-      ctx.json({ data: { attributes: { name: "msw user" } } }),
+      ctx.json({ data: { attributes: { name: "msw skill" } } }),
     );
   }),
-  rest.delete(/employees/, (_, res, ctx) => {
+  rest.delete(/skills/, (_, res, ctx) => {
     return res(ctx.status(201), ctx.json({ data: {} }));
   }),
 ];
 
-export const Empty: ComponentStory<typeof Employees> = () => (
+export const Empty: ComponentStory<typeof Skills> = () => (
   <Flex height="100%" width="100%" overflow="hidden">
     <Box backgroundColor={backgroundColor} flex="1 1" padding="40px">
-      <Employees />
+      <Skills />
     </Box>
   </Flex>
 );
@@ -43,7 +43,7 @@ Empty.parameters = {
   msw: {
     handlers: [
       ...mswHandlers,
-      rest.get(/employees/, (_, res, ctx) => {
+      rest.get(/skills/, (_, res, ctx) => {
         return res(ctx.json({ data: [] }));
       }),
     ],
@@ -51,20 +51,13 @@ Empty.parameters = {
 };
 
 // @todo skeleton state when data layer is added
-// export const Loading: ComponentStory<typeof Flex> = ({ ...props }) => (
-//   <Flex height="100%" width="100%" overflow="hidden">
-//     <Box backgroundColor={backgroundColor} flex="1 1" padding="40px">
-//       <EmployeePageLoadingLayout {...props} />
-//     </Box>
-//   </Flex>
-// );
 
-export const WithData: ComponentStory<typeof Employees> = () => {
+export const WithData: ComponentStory<typeof Skills> = () => {
   return (
     <BrowserRouter>
       <Flex height="100%" width="100%" overflow={"visible"}>
         <Box backgroundColor={backgroundColor} flex="1 1" padding="40px">
-          <Employees />
+          <Skills />
         </Box>
       </Flex>
     </BrowserRouter>
